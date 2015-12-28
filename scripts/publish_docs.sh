90ce9499fc85427ccb836a3f9890af41ff872dcc
Andrew McKenzie <andrew.mckenzie@electronifie.com>
2015-12-27 23:30:56 -0500

    Fix doc deploy script.

--- scripts/publish_docs.sh
+++ scripts/publish_docs.sh
@@ -16,7 +16,9 @@ if [ "$TRAVIS_REPO_SLUG" == "electronifie/accountifie-svc" ] && [ "$TRAVIS_NODE_
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/electronifie/accountifie-svc gh-pages > /dev/null
 
   cd gh-pages
-  find . -not -name '.git*' | xargs rm -r
+  mv .git ../.git_backup
+  rm -r *
+  mv ../.git_backup .git
   cp -rf $HOME/api-docs/* .
   git add -f .
   git commit -m "Updating autogen'd docs for travis build $TRAVIS_BUILD_NUMBER."
